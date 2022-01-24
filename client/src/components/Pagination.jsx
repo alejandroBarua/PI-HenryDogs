21cb644e76ed46f6eb5146e0c7c2390f0e844a8f
alejandroBarua <baruaalejandro95@gmail.com>
2022-01-24 18:38:09 -0300

    finished responsive pagination

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -28,33 +28,124 @@ const Pagination = () => {
 
 	const { total, page } = useSelector(state => state);
 
+	const dispatch = useDispatch();
+	const totalPages = Math.ceil(total / 8);
+
+	const handleOnNext = () => {
+	 if(page !== totalPages) dispatch(setPage(page+1));
+	}
+
+	const handleOnPrevious = () => {
+		if(page !== 1) dispatch(setPage(page-1));
+	}
+
 	return (
-		<Flex>
-			{
-				[...Array(total)].slice(0, Math.ceil(total / 8)).map((el, index) => (
-					<Item 
-						key={index} 
-						num={index+1}
-						page={page} />
-				))
-			}
-		</Flex>
+		<PaginationStyled>
+			<Flex>
+				{
+					(totalPages && totalPages !== 1) ? <p className={page !== 1 ? 'previous': 'previous-inactive'}
+											 onClick={handleOnPrevious}>Previous</p> : ''
+				}
+				{
+					totalPages <= 6 ? (
+						<>
+							{
+								[...Array(total)].slice(0, totalPages).map((el, index) => (
+									<Item 
+										key={index} 
+										num={index+1}
+										page={page} />
+								))
+							}
+						</>
+					)
+					: (
+						totalPages - page <=5 ?
+							(
+								<>
+									<Item num={1} page={page} />
+									<span>...</span>
+									{
+
+										[...Array(total)].slice(totalPages-6, totalPages).map((el, index) => (
+											<Item 
+												key={index} 
+												num={totalPages+index-5}
+												page={page} />
+										))
+									}
+								</>
+							)
+						: (
+							<>
+								<Item num={page} page={page} />
+								<Item num={page+1} page={page}/>
+								<Item num={page+2} page={page}/>
+								<span>...</span>
+								<Item num={totalPages-2} page={page} />
+								<Item num={totalPages-1} page={page} />
+								<Item num={totalPages} page={page} />
+							</>
+						)
+					)
+				}
+				{
+					(totalPages && totalPages !== 1) ? <p className={page !== totalPages ? 'next': 'next-inactive'}
+											onClick={handleOnNext}>Next</p> : ''
+				}
+			</Flex>
+		</PaginationStyled>
 	)
 }
 
 export default Pagination;
 
 
-const Flex = styled.div`
+const PaginationStyled = styled.div`
 
 	position: absolute;
 	bottom: -70px;
 	left: 0;
 	right: 0;
 
+
+	margin-top: 3rem;
+`
+
+const Flex = styled.div`
+
 	display: flex;
 	justify-content: center;
-	margin-top: 3rem;
+	align-items: center;
+
+	span{
+		padding: 0rem 0.2rem;
+	}
+
+	.next{
+		margin-left: 0.9rem;
+	}
+
+	.previous{
+		margin-right: 0.9rem;
+	}
+	
+	.next-inactive{
+		color: #c8c8c8;
+		margin-left: 0.9rem;
+	}
+
+	.previous-inactive{
+		color: #c8c8c8;
+		margin-right: 0.9rem;
+	}
+
+	.next:hover,
+	.previous:hover{
+		cursor: pointer;
+		color: ${({theme}) => theme.colorPrimary};
+	}
+
 `
 
 const ItemPage = styled.div`
